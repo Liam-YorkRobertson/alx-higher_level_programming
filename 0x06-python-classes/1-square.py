359eb44098cf1197268195b373983e9d507afb9f
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-30 14:58:09 -0700

    created class Square and added attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initializes a new square."""
+
+        self.__size = size
