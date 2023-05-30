be8a56b283509fbc6439b068ec317d99431a8158
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-30 15:28:53 -0700

    created class Square and added more functionality

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializes a new Square."""
+        """Initialize a new Square."""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
