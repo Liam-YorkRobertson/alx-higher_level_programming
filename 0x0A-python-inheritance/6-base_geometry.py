cf7ee7b4f355e0192da8a594a05c238b684089a7
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-12 16:34:50 -0700

    created a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a base geomery class BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    Represents base geometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
