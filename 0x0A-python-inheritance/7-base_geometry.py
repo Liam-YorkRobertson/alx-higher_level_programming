40006695341ed73978a42d70d423762c3b38369e
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-12 17:10:25 -0700

    create a class BaseGeometry (based on 6-base_geometry.py)

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a base geomery class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents base geometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
