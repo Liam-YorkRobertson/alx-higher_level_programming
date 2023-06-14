b899c3028ba6a3fc731a610b5c43c4198df0a3bd
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-13 17:30:05 -0700

    create a class Student that defines a student by

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,9 +10,9 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name #str
-        self.last_name = last_name #str
-        self.age = age #int
+        self.first_name = first_name  # str
+        self.last_name = last_name  # str
+        self.age = age  # int
 
     def to_json(self):
         return (self.__dict__)
