e9e5f76c5edea8c2fa4d0a49ceaa6eb9b6bfd2cc
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-13 18:14:59 -0700

    create a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Defines class Student.
+"""
+
+
+class Student:
+    """
+    Represent a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name  # str
+        self.last_name = last_name  # str
+        self.age = age  # int
+
+    def to_json(self, attrs=None):
+        """
+        Get a dictionary representation of the student.
+        """
+
+        if attrs is None:
+            return (self.__dict__)
+        return {attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """
+        Replace attributes of student.
+        """
+
+        for attr, value in json.items():
+            setattr(self, attr, value)
