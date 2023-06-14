a41b3344d032432089e3695957db01b9558cdb7a
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-13 17:17:55 -0700

    create a class Student that defines a student by

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines class Student.
+"""
+
+
+class Student:
+    """
+    Represent a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name #str
+        self.last_name = last_name #str
+        self.age = age #int
+
+    def to_json(self):
+        return (self.__dict__)
