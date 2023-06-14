5b6540bcae7f69188ff5452064f9175be420ec9e
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-13 17:51:20 -0700

    class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,14 +10,15 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name #str
-        self.last_name = last_name #str
-        self.age = age #int
+        self.first_name = first_name  # str
+        self.last_name = last_name  # str
+        self.age = age  # int
 
     def to_json(self, attrs=None):
         """
-        Get a dictionary representation of the student
+        Get a dictionary representation of the student.
         """
+
         if attrs is None:
             return (self.__dict__)
         return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
