0e2882d66043312c17607acc38b36b31c12e9775
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-18 23:30:40 -0700

    updated base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """
 Defines Base function.
 """
@@ -12,8 +13,40 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes Base function.
+        """
+
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns json representation of list_dictionaries
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Returns the list of the JSON string representation json_string.
+        """
+
+        filename = cls.__name__ + ".json"
+
+        if list_objs is None:
+            list_objs = []
+
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+
+        with open(filename, 'w') as file:
+            file.write(json_string)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -63,3 +63,15 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """
+        Returns dictionary representation of Square.
+        """
+
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
