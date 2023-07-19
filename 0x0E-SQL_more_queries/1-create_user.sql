3b32c8afe2c088b1bdc3a77fb69de820a155f1ae
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-07-19 08:30:50 -0700

    script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
--- script that creates the MySQL server user user_0d_1
-CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost'
-	WITH GRANT OPTION;
+-- Create the user user_0d_1 with all privileges and set the password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1 on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
