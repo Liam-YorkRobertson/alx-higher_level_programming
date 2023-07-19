dd758e3bbe8e44de34bb4c23311d3bc65b375dec
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-07-19 08:56:27 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 -- Script to list privileges of MySQL users user_0d_1 and user_0d_2
 
--- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
