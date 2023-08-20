b3c23e938b48f93065d6b722e1b075f3de853521
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-08-20 15:48:37 -0700

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,11 +4,15 @@
 script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument
 """
+
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
+    """
+    beep boop
+    """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
