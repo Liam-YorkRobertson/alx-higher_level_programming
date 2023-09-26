92c3ac777d838eef893db25b32fdcb9fefa08cba
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-09-26 15:55:59 -0700

    script that prints the number of movies where the character “Wedge Antilles” is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* script that prints the number of movies where the character “Wedge Antilles” is present */
+
+const request = require('request');
+const url = process.argv[2];
+const id = 18;
+
+request.get(url, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const hasWedgeAntilles = data.results.filter(function (film) {
+      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`);
+    });
+    console.log(hasWedgeAntilles.length);
+  }
+});
